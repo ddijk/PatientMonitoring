79ffb06c74e1e0f0a21eb2d73ba3e37a5ecc9fbd
ddijk <dick@dijkrosoft.nl>
2017-02-05 21:37:31 +0100

    rename feature file according to best practice

--- src/test/java/org/ucll/demo/service/Runner.java
+++ src/test/java/org/ucll/demo/service/Runner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
  * Created by dickdijk on 01/11/16.
  */
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"classpath:org/ucll/demo/service/ShowPatientDetails.feature"},
+@CucumberOptions(features = {"classpath:org/ucll/demo/service/show_patient_details.feature"},
         format = {"html:target/cucumber", "json:target/cucumber.json"}
 )
 public class Runner {
