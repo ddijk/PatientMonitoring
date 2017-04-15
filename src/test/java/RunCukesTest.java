17591d319667a4f131c46bb69059c1f19fa8b792
ddijk <dick@dijkrosoft.nl>
2017-04-15 20:52:21 +0200

    run Cucumber via maven's exec:java plugin

--- src/test/java/RunCukesTest.java
+++ src/test/java/RunCukesTest.java
@@ -1,11 +1,8 @@
-/**
- * Created by dickdijk on 14/04/2017.
- */
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features/cash_withdrawal.feature", glue = "step_definitions")
+//@RunWith(Cucumber.class)
+//@CucumberOptions(features = "src/test/resources/features/cash_withdrawal.feature", glue = "step_definitions")
 public class RunCukesTest { // NOSONAR SonarLint does not know about @RunWith(Cucumber.class)
 }
